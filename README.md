d89f9eb9f46c240b4a0a1cc9f4adf9d9888a5dfd
Markus Jais <markusjais@gmx.de>
2013-05-03 20:55:03 +0200

    empty repository

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a collection of small examples about how to use futures with the Scala 2.10. 
+
---
+++ project/scala-futures-examples.scala
@@ -0,0 +1,24 @@
+import sbt._
+import sbt.Keys._
+
+object scala_futures_examples extends Build {
+
+  lazy val scala_futures_examples = Project(
+    id = "scala-futures-examples",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "scala-futures-examples",
+      organization := "com.markusjais",
+      version := "0.1-SNAPSHOT",
+   	  scalaVersion := "2.10.1",
+      resolvers ++= Seq(
+            "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases"
+      ),
+
+      libraryDependencies ++= Seq(
+        "com.github.axel22" %% "scalameter" % "0.3" withSources()
+       )
+    )
+  )
+}
+
