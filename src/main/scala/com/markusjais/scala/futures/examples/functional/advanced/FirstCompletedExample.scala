dbc1b660d2b47e4983a2bb612189cf74efbd1f9f
Markus Jais <markus.jais@1und1.de>
2013-07-08 13:18:41 +0200

    fixed string interpolation

--- src/main/scala/com/markusjais/scala/futures/examples/functional/advanced/FirstCompletedExample.scala
+++ src/main/scala/com/markusjais/scala/futures/examples/functional/advanced/FirstCompletedExample.scala
@@ -13,7 +13,7 @@ object FirstCompletedExample extends App {
   val cacheFuture2 = Future { db.readUserNameFromCacheServer_2(42) }
 
   Future.firstCompletedOf(List(cacheFuture1, cacheFuture2)) onSuccess { // ignore failure here
-    case userName => println("user: $userName")
+    case userName => println(s"user: $userName")
   }
 
   // necessary in this dummy app to let future complete
